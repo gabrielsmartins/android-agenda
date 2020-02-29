44db594a6a00ea7fe1040f08f972bf55fb934d1f
Gabriel Martins <ga.smartins94@gmail.com>
2020-02-29 18:15:40 -0300

    Implementando View

--- app/src/main/java/br/com/alura/MainActivity.java
+++ app/src/main/java/br/com/alura/MainActivity.java
@@ -1,6 +1,30 @@
 package br.com.alura;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<String> alunos = Arrays.asList("Alex", "Fran", "José", "Gabriel", "Miguel", "Ana");
+        ListView lista = findViewById(R.id.activity_main_lista_de_alunos);
+        lista.setAdapter(new ArrayAdapter<>(this,
+                                                   android.R.layout.simple_list_item_1,
+                                                   alunos));
+
+    }
 }
